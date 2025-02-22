86aa2d50507bd5e9f040def130bf1755fbcda4c5
sokkhengg <sokhengteangg@gmail.com>
2025-02-21 20:16:31 -0800

    dealing with slow data sources

--- app/about/projects/page.js
+++ app/about/projects/page.js
@@ -1,5 +1,21 @@
-export default function ProjectPage() {
+// export const dynamic = 'force-dynamic'
+
+import { Suspense } from "react"
+import ProjectList from "./components/project-list"
+import ProjectListLoading from "./components/project-list-loading"
+
+export default async function ProjectPage() {
+    
+    
     return (
-        <>This is project page nested inside about page</>
+        <div className="p-20">
+            <h1 className="mb-8 text-xl">Projects</h1>
+            <div className="mb-8">Hello, this is the list of my projects</div>
+            <Suspense fallback={<ProjectListLoading/>}>
+                <ProjectList/>
+
+            </Suspense>
+
+        </div>
     )
 }
